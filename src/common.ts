d4acc68416f1de5a7db0170032decf79f017fcf3
Noam <noam@fireflymigration.com>
2020-12-29 15:45:34 +0200

    adjusted for latest version

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,4 @@
-import { Context, CookieBasedJwt } from "@remult/core";
+import { Context } from "@remult/core";
+import { CookieBasedJwt } from '@remult/core/src/cookieBasedJwt';
 export const context = new Context();
 export const authorization = new CookieBasedJwt(context);
